9f9720232749c1492eab4de10c52e77f0d327db3
Zackery Fathi <zfathi@umich.edu>
2023-10-21 14:48:29 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <title>asanaCoach</title>
+   <script scr="script.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id = "topHeaderRow">
+        <ul>
+            <li><a href="#section1">Section 1: </a></li>
+            <li><a href="#section2">Section 2: </a></li>
+            <li><a href="#section3">Section 3: </a></li>
+            <li><a href="#section4">Section 4: </a></li>
+            <li><a href="#section5">Section 5: </a></li>
+        </ul>
+    </div>
+
+    <section id="section1">
+    </section>
+
+</body>
+</html>
